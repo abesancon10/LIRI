9ea0c0e185d0998b46e3892da9ebd74cced7b965
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-05-04 17:58:47 -0500

    added const for the process.argv[2] and [3] followed by a switch-case for the options that can be used.

--- liri.js
+++ liri.js
@@ -1,2 +1,24 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+
+var input = process.argv[2];
+var value = process.argv[3];
+console.log("hello there");
+
+switch (action) {
+  case "concert-this":
+    concert();
+
+  case "concert-this":
+    
+
+  case "spotify-this-song":
+    spotify();
+
+  case "movie-this":
+    movie();
+
+  case "do-what-it-says":
+    doIt();
+}
+console.log("General Kenobi!");
