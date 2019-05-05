dacb902f737f01796da9bf6c17f6500c5ad92f02
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-05-04 19:05:26 -0500

    code is pulling from the omdb api however it's pulling not what I'm inputting. Going to debug.

--- liri.js
+++ liri.js
@@ -1,49 +1,51 @@
 require("dotenv").config();
-const axios = require("axios");
-const keys = require("./keys.js");
+var axios = require("axios");
+var keys = require("./keys.js");
 
-const input = process.argv;
-
-console.log("Hello there");
+var action = process.argv[2];
+var entry = process.argv[3];
 
 switch (action) {
   case "concert-this":
     concert();
-
-  case "concert-this":
-
+    console.log(concert)
   case "spotify-this-song":
     spotify();
-
+    console.log(spotify)
   case "movie-this":
     movie();
 
-  case "do-what-it-says":
-    doIt();
+//   case "do-what-it-says":
+//     doIt();
+//     console.log(doIt)
 }
 
 function movie() {
-  let movieInput = process.argv;
-  let movieName = "";
-  for (var i = 2; i < movieInput.length; i++) {
-    if (i > 2 && i < nodeArgs.length) {
-      movieName = movieName + "+" + nodeArgs[i];
+  var movieName = "";
+
+  for (var i = 2; i < entry.length; i++) {
+    if (i > 2 && i < entry.length) {
+      movieName = movieName + "+" + entry[i];
     } else {
-      movieName += nodeArgs[i];
+      movieName += entry[i];
     }
+
+    var movieURL =
+      "http://www.omdbapi.com/?t=" +
+      movieName +
+      "&y=&plot=short&apikey=trilogy";
+
+    axios.get(movieURL).then(function(err, response) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(
+        "Title: " +
+          response.data.Title +
+          "\n" +
+          "Release Year: " +
+          response.data.Year
+      );
+    });
   }
-  let movieURL =
-    "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-  console.log(movieURL);
-
-  axios.get(movieURL).then(function(response) {
-    console.log("Title: " + response.data.Title) +
-      "\n" +
-      "Release Year: " +
-      response.data.Year +
-      "\n" +
-      "Rating: " +
-      response.data.Ratings;
-  });
 }
